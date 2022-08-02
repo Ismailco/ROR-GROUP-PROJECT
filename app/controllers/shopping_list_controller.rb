dca869d28d9050bf141035ca7c85a004c008037a
Ismail Courr <contact@ismailcourr.ml>
2022-08-02 14:21:45 +0100

    create shopping list page

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,6 @@
+class ShoppingListController < ApplicationController
+
+    def index; end
+
+    def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "users#show"
+  get 'general_shopping_list', to: 'shopping_list#index'
   resources :users
   resources :foods
   resources :recipes
   resources :recipe_foods
+  resources :shopping_list
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
