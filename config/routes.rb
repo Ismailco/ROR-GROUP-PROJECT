d66a4653c7b573100eafecab102e18ef47b4f5be
Adekunle Ismail <adismail4@gmail.com>
2022-08-03 14:50:28 +0100

    Add recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :public_recipes, only: [:index]
   devise_for :users
   root to: "users#show"
   resources :users
   resources :foods
-  resources :recipes
-  resources :recipe_foods
+  
+  resources :recipes do
+    resources :recipe_foods    
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
