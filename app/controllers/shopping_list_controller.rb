c671fb2ac427e059e8eacde484a730aba81fc81d
Ismail Courr <contact@ismailcourr.ml>
2022-08-03 16:48:29 +0100

    Create shopping list foods

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,8 +1,7 @@
 class ShoppingListController < ApplicationController
   def index
-     foods = Food.includes(:recipes).where(user: current_user)
-    @food_needed = foods.filter { |food| food.recipes.blank? }
-    @food_total = @food_needed.count
-    @total_value = @food_needed.sum(:price)
+    @food_needed = Food.includes(:recipe_foods).where(user_id: current_user.id, recipe_foods: { recipe_id: nil })
+    @food_needed.count == 0 ? @food_amount = 0 : @food_amount = @food_needed.count
+    @food_needed.sum(:price) == 0 ? @total_value = 0 : @total_value = @food_needed.sum(:price)
   end
 end
