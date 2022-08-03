00e13903a7d52b2ad9db221b0425196e893b5e02
Ismail Courr <contact@ismailcourr.ml>
2022-08-03 16:29:57 +0100

    Create shopping list logic

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,5 +1,8 @@
 class ShoppingListController < ApplicationController
-  def index; end
-
-  def show; end
+  def index
+     foods = Food.includes(:recipes).where(user: current_user)
+    @food_needed = foods.filter { |food| food.recipes.blank? }
+    @food_total = @food_needed.count
+    @total_value = @food_needed.sum(:price)
+  end
 end
