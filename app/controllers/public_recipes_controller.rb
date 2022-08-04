eda45185098b6a96adb651130e826526c3baf38f
Adekunle Ismail <adismail4@gmail.com>
2022-08-04 14:18:01 +0100

    Add desc order to public recipe index

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.includes(:recipe_foods, :user).where('public = true')
+    @public_recipes = Recipe.includes(:recipe_foods, :user).where('public = true').order(created_at: :desc)
   end    
 end
