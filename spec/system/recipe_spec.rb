20479a17e95ba9980514b02e8519458f00b3263e
Adekunle Ismail <adismail4@gmail.com>
2022-08-05 12:59:55 +0100

    Add recipe test

---
+++ spec/system/recipe_spec.rb
@@ -0,0 +1,21 @@
+ require 'rails_helper'
+
+RSpec.describe 'Recipes#index', type: :system do
+  before do
+    driven_by(:rack_test)
+
+    login_as(create(:user))
+
+    visit recipes_path
+  end
+
+  it 'has a header for the list' do
+    expect(page).to have_content('All recipes')
+  end
+
+  it 'has a content New recipe' do
+    expect(page).to have_content('New recipe')
+  end
+
+end
+
